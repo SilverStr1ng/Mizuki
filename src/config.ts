dc38fb04c07b738fd374564e174d375e0a4b4cf5
刘金昭 <953958973@qq.com>
2025-12-11 14:01:15 +0800

    chore: 统一标题

--- src/config.ts
+++ src/config.ts
@@ -49,7 +49,7 @@ export const siteConfig: SiteConfig = {
 	// 顶栏标题配置
 	navbarTitle: {
 		// 顶栏标题文本
-		text: "落叶Rakuyou's Blog",
+		text: "落叶Rakuyou's Labyrinth",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.png
 		icon: "assets/home/home.png",
 	},
@@ -135,7 +135,7 @@ export const siteConfig: SiteConfig = {
 
 		homeText: {
 			enable: true, // 在主页显示自定义文本
-			title: "雲の切れ間に、月光の花", // 主页横幅主标题
+			title: "雲の切れ間に", // 主页横幅主标题
 
 			subtitle: [
 				"秋風にたなびく雲の絶え間より、漏れ出づる月の影のさやけさ"
